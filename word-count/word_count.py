819803556a1c942e858cc11ccfbe00546fa08638
Daniel Ise <ise.daniel@gmail.com>
2023-03-15 19:51:51 -0300

    word-count done

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,2 +1,9 @@
+import re
+
+def split_words(sentence):
+    # re means letter apostrophe letter or any alphanumeric
+    return re.findall("[a-z]+'[a-z]+|[a-z0-9]+", sentence.lower())
+
 def count_words(sentence):
-    pass
+    words = split_words(sentence)
+    return {word: words.count(word) for word in words}
