1ef937d774dd7bef599bbee72efe0f542e45ab4e
Daniel Ise <ise.daniel@gmail.com>
2023-03-17 21:11:43 -0300

    yacht done

--- yacht/yacht.py
+++ yacht/yacht.py
@@ -1,20 +1,23 @@
 # Score categories.
 # Change the values as you see fit.
-YACHT = lambda a: sum(a) == a*5
-ONES = lambda a: a.count(1)
-TWOS = lambda a: a.count(2) * 2
-THREES = lambda a: a.count(3) * 3
-FOURS = lambda a: a.count(4) * 4
-FIVES = lambda a: a.count(5) * 5
-SIXES = lambda a: a.count(6) * 6
-FULL_HOUSE = lambda a: check_equality(a[:3]) and check_equality(a[3:])
-FOUR_OF_A_KIND = None
-LITTLE_STRAIGHT = None
-BIG_STRAIGHT = None
-CHOICE = None
+from functools import wraps
 
-def check_equality(list):
-    return len(set(list)) == 1
+YACHT = lambda d: 50 if len(set(d)) == 1 else 0
+ONES = lambda d: d.count(1)
+TWOS = lambda d: d.count(2) * 2
+THREES = lambda d: d.count(3) * 3
+FOURS = lambda d: d.count(4) * 4
+FIVES = lambda d: d.count(5) * 5
+SIXES = lambda d: d.count(6) * 6
+FULL_HOUSE = lambda d: sum(d) if count_repeated(d) == [2, 3] else 0
+FOUR_OF_A_KIND = lambda d: sorted(d)[1] * 4 if count_repeated(d)[-1] >= 4 else 0
+LITTLE_STRAIGHT = lambda d: 30 if sorted(d) == [1, 2, 3, 4, 5] else 0
+BIG_STRAIGHT = lambda d: 30 if sorted(d) == [2, 3, 4, 5, 6] else 0
+CHOICE = sum
+
+def count_repeated(dices):
+    return sorted([dices.count(dice) for dice in set(dices)])
 
 def score(dice, category):
-    print(dice, category)
+    return category(dice)
+
