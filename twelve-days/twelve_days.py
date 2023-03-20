5c5b42c9419526c72de4f717223395cf77caf0cb
Daniel Ise <ise.daniel@gmail.com>
2023-03-20 17:04:07 -0300

    improved twelve-days

--- twelve-days/twelve_days.py
+++ twelve-days/twelve_days.py
@@ -14,32 +14,28 @@ DAYS = [
 ]
 
 GIFTS = [
-    "and a Partridge in a Pear Tree.",
-    "two Turtle Doves,",
-    "three French Hens,",
-    "four Calling Birds,",
-    "five Gold Rings,",
-    "six Geese-a-Laying,",
-    "seven Swans-a-Swimming,",
-    "eight Maids-a-Milking,",
-    "nine Ladies Dancing,",
-    "ten Lords-a-Leaping,",
-    "eleven Pipers Piping,",
-    "twelve Drummers Drumming,",
+    "a Partridge in a Pear Tree",
+    "two Turtle Doves",
+    "three French Hens",
+    "four Calling Birds",
+    "five Gold Rings",
+    "six Geese-a-Laying",
+    "seven Swans-a-Swimming",
+    "eight Maids-a-Milking",
+    "nine Ladies Dancing",
+    "ten Lords-a-Leaping",
+    "eleven Pipers Piping",
+    "twelve Drummers Drumming",
 ]
-
-PARTRIDGE = "a Partridge in a Pear Tree."
-
-def verse(day, gift):
-    return f"On the {day} day of Christmas my true love gave to me: {gift}"
 
 def recite(start_verse, end_verse):
     verses = []
 
     for i in range(start_verse - 1, end_verse):
         if i == 0:
-            verses.append(verse(DAYS[i], PARTRIDGE))
+            verses.append(f"On the {DAYS[i]} day of Christmas my true love gave to me: {GIFTS[0]}.")
             continue
-        verses.append(verse(DAYS[i], " ".join(GIFTS[:i+1][::-1])))
+        verses.append(f"On the {DAYS[i]} day of Christmas my true love gave to me: {', '.join(GIFTS[i:0:-1])}, and {GIFTS[0]}.")
 
     return verses
+
